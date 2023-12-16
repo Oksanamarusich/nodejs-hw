a041636b851dd54cc699e5abdc243ac621c11f13
Oksanamarusich <oksana.m229@gmail.com>
2023-12-16 12:28:28 +0200

    fix bags

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -49,37 +49,56 @@ const updateById = async (req, res, next) => {
     try {
         const { error } = contactUpdateSchema.validate(req.body);
         if (error) {
-            throw HttpError(400, "missing fields");
+            throw HttpError(400, error.message);
         }
         const { contactId } = req.params;
-        const result = await contactsService.updateContact(contactId, req.body)
+        const result = await contactsService.updateContact(contactId, req.body);
         if (!result) {
-            throw HttpError(404);
+            throw HttpError(404, "Not found");
         }
-        res.json(result);
 
-     }
+        res.json(result);
+    }
     catch (error) {
         next(error);
     }
-  
 }
 
+
+// const updateById = async (req, res, next) => {
+//     try {
+//         const { error } = contactUpdateSchema.validate(req.body);
+//         if (error) {
+//             throw HttpError(400, error.message);
+//         }
+//         const { contactId } = req.params;
+//         const result = await contactsService.updateContact(contactId, req.body)
+//         if (!result) {
+//             throw HttpError(404);
+//         }
+//         res.json(result);
+
+//      }
+//     catch (error) {
+//         next(error);
+//     }
+  
+// }
 const deleteById = async (req, res, next) => {
     try {
         const { contactId } = req.params;
-        const result = contactsService.removeContact(contactId);
-        
+        const result = await contactsService.removeContact(contactId);
         if (!result) {
-            throw HttpError(404);
-            }
+            throw HttpError(404, "Not found");
+        }
 
-        res.json({ message: "contact deleted" });
+        res.json({
+            message: "contact deleted"
+        })
     }
     catch (error) {
         next(error);
     }
-  
 }
 
 
--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -1,9 +1,12 @@
 import Joi from "joi";
 
  export const contactAddSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().pattern(/^[0-9]{3}-[0-9]{2}-[0-9]{2}$/).required()
+     name: Joi.string().required()
+         .messages({"any.required": "missing required name field"}),
+     email: Joi.string().email().required()
+    .messages({ "any.required": "missing required email field"}),
+     phone: Joi.string().pattern(/^[0-9]{3}-[0-9]{2}-[0-9]{2}$/).required()
+         .messages({ "any.required": "missing required phone field" })
     
 });
 
