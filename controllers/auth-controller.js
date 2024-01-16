adb8096259683662658607c4fe014b6c6f0b33e4
Oksana Marusich <oksana.m229@gmail.com>
2024-01-16 21:37:20 +0200

    add settings

---
+++ controllers/auth-controller.js
@@ -0,0 +1,149 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { nanoid } from "nanoid";
+import "dotenv/config";
+import gravatar from "gravatar";
+import path from "path";
+import fs from "fs/promises";
+import Jimp from "jimp";
+
+import User, { userSignupSchema, userSigninSchema } from "../models/User.js";
+
+import { HttpError, sendEmail } from "../helpers/index.js";
+
+const { JWT_SECRET, BASE_URL } = process.env;
+
+const avatarsPath = path.resolve("public", "avatars");
+
+const signup = async (req, res, next) => {
+    try {
+        const { error } = userSignupSchema.validate(req.body);
+        if (error) {
+            throw HttpError(400, error.message);
+        }
+
+        const { email, password} = req.body;
+        const user = await User.findOne({ email });
+        if (user) {
+            throw HttpError(409, "Email in use");
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+        const verificationToken = nanoid();
+        const avatarURL = gravatar.url(email);
+        const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL, verificationToken});
+        const verifyEmail = {
+            to: email,
+        subject: "Verify email",
+        html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click to verify email</a>`
+        }
+
+        await sendEmail(verifyEmail);
+
+        res.status(201).json({
+            user: ({
+                email: newUser.email,
+                subscription: newUser.subscription,
+               
+            })
+            
+        })
+    }
+    catch (error) {
+        next(error)
+    }
+};
+
+const signin = async (req, res, next) => {
+    try {
+        const { error } = userSigninSchema.validate(req.body);
+        if (error) {
+            throw HttpError(400, error.message);
+        }
+
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw HttpError(401, "Email or password is wrong");
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            throw HttpError(401, "Email or password is wrong");
+        }
+
+        const { _id: contactId } = user;
+        const payload = {
+            contactId
+        };
+
+        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+        await User.findByIdAndUpdate(contactId, { token });
+        
+        res.json({
+            token,
+            user: ({
+                email: user.email,
+                subscription: user.subscription
+            })
+             
+        })
+    }
+    catch (error) {
+        next(error);
+    }
+};
+
+const getCurrent = async (req, res) => {
+    
+    const { email, subscription } = req.user;
+    
+    res.json({
+        email,
+        subscription
+    });
+    
+};
+
+const signout = async (req, res) => {
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, { token: "" });
+
+    res.status(204).json();
+};
+
+const updateAvatar = async (req, res, next) => {
+    try {
+       if (!req.file) {
+          throw HttpError(404, "File not found");  
+        }
+        
+        const { _id } = req.user;
+        const { path: oldPath, filename } = req.file;
+        const resultUpload = path.join(avatarsPath, filename);
+        const img = await Jimp.read(oldPath)
+        await img.autocrop()
+            .cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER || Jimp.VERTICAL_ALIGN_MIDDLE)
+            .writeAsync(oldPath)
+        await fs.rename(oldPath, resultUpload);
+        const avatarURL = path.join("avatars", filename);
+        await User.findByIdAndUpdate(_id, { avatarURL });
+
+        res.json({
+            avatarURL,
+        })
+    }
+    catch (error) {
+        next(error)
+    }
+};
+    
+
+
+export default {
+    signup,
+    signin, 
+    getCurrent,
+    signout,
+    updateAvatar
+}
